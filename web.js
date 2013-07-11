d7638933cc33dbdf1bb10c4dfc1a55d097240667
ti thanh <tithanh@gmail.com>
2013-07-11 00:35:49 +0000

    read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require("fs");
+var st = fs.readFileSync("index.html").toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(st);
 });
 
 var port = process.env.PORT || 5000;
