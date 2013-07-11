0aaf0b245d318baf7896c1bfca542885b9af527e
ti thanh <tithanh@gmail.com>
2013-07-11 06:51:23 +0000

    hw3 part 3

--- grader.js
+++ grader.js
@@ -27,6 +27,8 @@ var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
+var rest = require('restler');
+
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -61,14 +63,54 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var buildfn = function(checksfile) {
+    var response2console = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+	    process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        } else {
+//            console.error("Read %s", result);
+	    $ = cheerio.load(result);
+	    var checks = loadChecks(checksfile).sort();
+	    var out = {};
+	    for(var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+	    }
+	    console.error(out);
+        }
+    };
+    return response2console;
+};
+
+var checkHtmlUrl = function(html, checksfile) {
+console.error(">> %s", html);
+    $ = cheerio.load(html);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
+    }
+    return out;
+};
+
 if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-f, --file [html_file]', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url [html_url]', 'URL to index.html' )
 	.parse(process.argv);
+console.log(program.url);
+//console.log(rest.get(program.url).on('complete', response2console) );
+if(program.url==null){
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+} else {
+    var response2console = buildfn(program.checks);
+    rest.get(program.url).on('complete', response2console);
+}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
